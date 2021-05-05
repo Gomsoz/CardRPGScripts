f6a4ca5427fea9a9849b737cda99a830c1e43a91
Gomsoz <superduper0613@naver.com>
2021-05-05 22:23:16 +0900

    Improved Damage Text
    데미지 텍스트가 1초동안 표기되도록 개선하였음.

--- UI/GameScene/Scene/UI_Status.cs
+++ UI/GameScene/Scene/UI_Status.cs
@@ -7,6 +7,8 @@ public class UI_Status : UI_Scene
 {
     GameObject m_damageUI;
 
+    int textTime = 0;
+
     public override void Init()
     {
         base.Init();
@@ -22,5 +24,22 @@ public class UI_Status : UI_Scene
     public void ChangeDamageText(GameObject textUI, int value)
     {
         textUI.GetComponent<Text>().text = value.ToString();
+
+        if (textTime > 0)
+            textTime = 0;
+        else
+            StartCoroutine(StartDamageText(textUI));
+
+    }
+
+    IEnumerator StartDamageText(GameObject textUI)
+    {
+        while(textTime < 10)
+        {
+            yield return new WaitForSeconds(0.1f);
+            textTime++;
+        }
+        textUI.GetComponent<Text>().text = "";
+        textTime = 0;
     }
 }
